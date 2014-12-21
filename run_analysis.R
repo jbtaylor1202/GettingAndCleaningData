f67dcd97856829a0d763162871ae81a7e7d7a393
unknown <joet@MBP-SP04332.eis2win.co.uk>
2014-12-21 20:26:02 +0000

    run_analysis.R FINAL

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 #Ensure that zipped file had been downloaded and extracted to your working directory 
-#before running (do not set working directory to the extracted folder!)
+#before running (do not set working directory to the extracted folder!). The extracted folder should be named `UCI HAR Dataset'
+#if not already done so.
 
 
 #Ensure that 'dplyr' package has been installed before running this script.
@@ -8,19 +9,17 @@
 #This package can be obtained by running the command: install.packages('reshape2')
 
 library('dplyr')
-library('tidyr')
+library('reshape2')
 
 #Get working directory path
 directory<-getwd()
 
-#Import all data
+#Import all required data
 activity_labels<-read.table(paste(directory,"/","UCI HAR Dataset/activity_labels.txt",sep = ""))
 features <- read.table(paste(directory,"/","UCI HAR Dataset/features.txt",sep = ""))
-
 subject_test <- read.table(paste(directory,"/","UCI HAR Dataset/test/subject_test.txt",sep = ""))
 X_test <- read.table(paste(directory,"/","UCI HAR Dataset/test/X_test.txt",sep = ""))
 y_test <- read.table(paste(directory,"/","UCI HAR Dataset/test/y_test.txt",sep = ""))
-
 subject_train <- read.table(paste(directory,"/","UCI HAR Dataset/train/subject_train.txt",sep = ""))
 X_train <- read.table(paste(directory,"/","UCI HAR Dataset/train/X_train.txt",sep = ""))
 y_train <- read.table(paste(directory,"/","UCI HAR Dataset/train/y_train.txt",sep = ""))
@@ -31,10 +30,12 @@ MergeTest<-cbind(X_test,subject_test, y_test)
 TrainAndTest<-rbind(MergeTrain,MergeTest)
 
 #STEP 4 - Appropriately labels the data set with descriptive variable names.
-#This was compelted before step 3 and 4 to allow the variables containing mean and standard deviation to be slected
+#This was compelted before step 2 and 3 to allow the variables containing mean and standard deviation to be selected
 features2<-as.character(features[,2])
+#ensure there are no duplicate names:
 features3<-make.unique(features2)
 colnames(TrainAndTest)<-features3
+#Add names to the remaining columns
 colnames(TrainAndTest)[562]<-'subject'
 colnames(TrainAndTest)[563]<-"activity"
 
@@ -56,4 +57,5 @@ melt1<-melt(selectedCols,id.vars = c('subject','activity'))
 tidy1<- melt1 %>% group_by(subject,activity,variable) %>% summarise_each(funs(mean))
 names(tidy1)<-c("Subject","Activity","Variable","Mean")
 
+#Create output (tidy) data file to working directory
 write.table(x = tidy1,row.names = FALSE,file = 'TidyData.txt')
